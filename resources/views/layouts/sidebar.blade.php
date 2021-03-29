d3652bdb0c7378186ad502a154743fe666ecd5cf
ridhoajibx <ridhoajibx@gmail.com>
2021-03-29 15:23:14 +0700

    [ridho] - installing spatie package & create assignPermission & role

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -3,26 +3,44 @@
         <header class="font-medium text-gray-400 uppercase text-xs px-2">
             Main
         </header>
-        <a class="block py-2 px-2 text-gray-200" href="#">Dashboard</a>
+        <a class="block py-2 px-2 text-gray-200" href="/">Home</a>
+        <a class="block py-2 px-2 text-gray-200" href="/dashboard">Dashboard</a>
     </div>
-    <div class="mb-8">
-        <header class="font-medium text-gray-400 uppercase text-xs px-2">
-            Playlists
-        </header>
-        <a class="block py-2 px-2 text-gray-200" href="#">Create</a>
-        <a class="block py-2 px-2 text-gray-200" href="#">Table</a>
-    </div>
-    <div class="mb-8">
-        <header class="font-medium text-gray-400 uppercase text-sm px-2">
-            Category
-        </header>
-        <a class="block py-2 px-2 text-gray-200" href="#">Create</a>
-        <a class="block py-2 px-2 text-gray-200" href="#">Table</a>
-    </div>
-    <div class="mb-8">
-        <header class="font-medium text-gray-400 uppercase text-sm px-2">
-            Users
-        </header>
-        <a class="block py-2 px-2 text-gray-200" href="#">Table</a>
-    </div>
+    @if (Auth::user()->can('create playlists'))
+        <div class="mb-8">
+            <header class="font-medium text-gray-400 uppercase text-sm px-2">
+                Playlist
+            </header>
+            <a class="block py-2 px-2 text-gray-200" href="#">Create</a>
+            <a class="block py-2 px-2 text-gray-200" href="#">Table</a>
+        </div>
+    @endif
+    @if (Auth::user()->can('create tags'))
+        <div class="mb-8">
+            <header class="font-medium text-gray-400 uppercase text-sm px-2">
+                Category
+            </header>
+            <a class="block py-2 px-2 text-gray-200" href="#">Create</a>
+            <a class="block py-2 px-2 text-gray-200" href="#">Table</a>
+        </div>
+    @endif
+    @if (Auth::user()->hasRole('admin'))    
+        <div class="mb-8">
+            <header class="font-medium text-gray-400 uppercase text-sm px-2">
+                Users
+            </header>
+            <a class="block py-2 px-2 text-gray-200" href="#">Table</a>
+        </div>
+    @endif
+
+    <!-- Authentication -->
+    <form method="POST" action="{{ route('logout') }}">
+        @csrf
+
+        <a class="block py-2 px-2 text-gray-200" href="route('logout')"
+                onclick="event.preventDefault();
+                            this.closest('form').submit();">
+            {{ __('Log out') }}
+        </a>
+    </form>
 </div>
