de2218d30e8efd2f4888a168588c5056f64a04fd
ridhoajibx <ridhoajibx@gmail.com>
2021-03-29 14:40:03 +0700

    [ridho] - create model and migration to video, playlist, and tags

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,28 @@
+<div class="w-1/5 min-h-screen bg-gray-900 px-4 py-6">
+    <div class="mb-8">
+        <header class="font-medium text-gray-400 uppercase text-xs px-2">
+            Main
+        </header>
+        <a class="block py-2 px-2 text-gray-200" href="#">Dashboard</a>
+    </div>
+    <div class="mb-8">
+        <header class="font-medium text-gray-400 uppercase text-xs px-2">
+            Playlists
+        </header>
+        <a class="block py-2 px-2 text-gray-200" href="#">Create</a>
+        <a class="block py-2 px-2 text-gray-200" href="#">Table</a>
+    </div>
+    <div class="mb-8">
+        <header class="font-medium text-gray-400 uppercase text-sm px-2">
+            Category
+        </header>
+        <a class="block py-2 px-2 text-gray-200" href="#">Create</a>
+        <a class="block py-2 px-2 text-gray-200" href="#">Table</a>
+    </div>
+    <div class="mb-8">
+        <header class="font-medium text-gray-400 uppercase text-sm px-2">
+            Users
+        </header>
+        <a class="block py-2 px-2 text-gray-200" href="#">Table</a>
+    </div>
+</div>
