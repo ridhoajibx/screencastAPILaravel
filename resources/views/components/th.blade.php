09fd1f66daf78f908516b17bbaa5b77781acc430
ridhoajibx <ridhoajibx@gmail.com>
2021-03-31 14:45:01 +0700

    [ridho] - refactor th, td styles border

--- resources/views/components/th.blade.php
+++ resources/views/components/th.blade.php
@@ -1,3 +1,3 @@
-<th {{ $attributes->merge([ 'class' => 'border-b border-gray-400 text-sm text-left py-2 px-2' ]) }}>
+<th {{ $attributes->merge([ 'class' => 'border-b-2 border-gray-300 text-sm text-left py-2 px-2' ]) }}>
     {{ $slot }}
 </th>
