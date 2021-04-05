644d0ae7ba126f066370e5e32d97363b502ccd2f
ridhoajibx <ridhoajibx@gmail.com>
2021-04-05 20:03:42 +0700

    [ridho] - create MyPlaylistController to fetch playlist that that has been purchased

--- routes/api.php
+++ routes/api.php
@@ -3,25 +3,27 @@
 use App\Http\Controllers\Auth\MeController;
 use App\Http\Controllers\Order\CartController;
 use App\Http\Controllers\Order\OrderController;
+use App\Http\Controllers\Screencast\MyPlaylistController;
 use App\Http\Controllers\Screencast\PlaylistController;
 use App\Http\Controllers\Screencast\VideoController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('playlists')->group(function () {
-    Route::get('', [PlaylistController::class, 'index']);
-    Route::get('{playlist:slug}', [PlaylistController::class, 'show']);
-
-    Route::get('{playlist:slug}/videos', [VideoController::class, 'index']);
-});
-
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('me', MeController::class);
+    Route::get('playlists/mine', MyPlaylistController::class);
     Route::get('playlists/{playlist:slug}/{video:episode}', [VideoController::class, 'show']);
 
     Route::get('carts', [CartController::class, 'index']);
     Route::post('add-to-cart/{playlist:slug}', [CartController::class, 'store']);
 
     Route::post('order/create', [OrderController::class, 'store']);
+});
+
+Route::prefix('playlists')->group(function () {
+    Route::get('', [PlaylistController::class, 'index']);
+    Route::get('{playlist:slug}', [PlaylistController::class, 'show']);
+
+    Route::get('{playlist:slug}/videos', [VideoController::class, 'index']);
 });
 
 Route::post('notification-handler', [ OrderController::class, 'notificationHandler' ]);
